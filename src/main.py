0c137eff8c1ebf7990f9d2d263d7d8bdf90337cb
misagh.asgari <misagh.asgari@gmail.com>
2022-06-27 16:34:04 +0300

    Add main file with training loop

---
+++ src/main.py
@@ -0,0 +1,101 @@
+#  MIT License
+#
+#  Copyright (c) 2022 Misagh Asgari
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+
+from base import hyperparameters
+from networks import simple_gan
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+params = hyperparameters.GAN(lr=3e-4, z_dim=64, image_dim=28 * 28 * 1, batch_size=32, num_epochs=1)
+
+disc, gen = simple_gan.gan(params.image_dim, params.z_dim)
+disc.to(device)
+gen.to(device)
+
+fixed_noise = torch.randn((params.batch_size, params.z_dim)).to(device)
+transforms = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,)),
+    ]
+)
+
+dataset = datasets.MNIST(root="../datasets/", transform=transforms, download=True)
+loader = DataLoader(dataset, batch_size=params.batch_size, shuffle=True)
+opt_disc = optim.Adam(disc.parameters(), lr=params.lr)
+opt_gen = optim.Adam(gen.parameters(), lr=params.lr)
+criterion = nn.BCELoss()
+writer_fake = SummaryWriter(f"../runs/GAN_MNIST/fake")
+writer_real = SummaryWriter(f"../runs/GAN_MNIST/real")
+step = 0
+
+for epoch in range(params.num_epochs):
+    for batch_idx, (real, _) in enumerate(loader):
+        real = real.view(-1, 784).to(device)
+        batch_size = real.shape[0]
+
+        noise = torch.randn(batch_size, params.z_dim).to(device)
+        fake = gen(noise)
+        disc_real = disc(real).view(-1)
+        lossD_real = criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = disc(fake).view(-1)
+        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+        lossD = (lossD_real + lossD_fake) / 2
+        disc.zero_grad()
+        lossD.backward(retain_graph=True)
+        opt_disc.step()
+
+        output = disc(fake).view(-1)
+        lossG = criterion(output, torch.ones_like(output))
+        gen.zero_grad()
+        lossG.backward()
+        opt_gen.step()
+
+        if batch_idx == 0:
+            print(
+                f"Epoch [{epoch}/{params.num_epochs}] \ "
+                f"Epoch D: {lossD:.4f}, Loss G: {lossG:.4f}"
+            )
+
+            with torch.no_grad():
+                fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
+                data = real.reshape(-1, 1, 28, 28)
+                img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
+                img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+
+                writer_fake.add_image(
+                    "Mnist Fake Images", img_grid_fake, global_step=step,
+                )
+
+                writer_real.add_image(
+                    "Mnist Real Images", img_grid_real, global_step=step
+                )
